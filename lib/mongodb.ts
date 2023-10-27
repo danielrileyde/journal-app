73308d3223359d87de7fe6d31635965a2caac677
Daniel Riley <dann.riley@icloud.com>
2023-10-27 15:45:35 +0200

    TS mongo global

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -10,17 +10,23 @@ const options = {};
 let client;
 let clientPromise;
 
+let globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<MongoClient>;
+};
+if (!globalWithMongo._mongoClientPromise) {
+  client = new MongoClient(uri);
+  globalWithMongo._mongoClientPromise = client.connect();
+}
+clientPromise = globalWithMongo._mongoClientPromise;
+
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR ( Hot Module Replacement).
-  //@ts-ignore
-  if (!global._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    //@ts-ignore
-    global._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  //@ts-ignore
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   //In production mode, it's best to not use a gobal varibale.
   client = new MongoClient(uri, options);
